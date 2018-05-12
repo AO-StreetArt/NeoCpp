9ac69b88c57d43d9015448de7131fd69e9c5710c
Alex Barry <aostreetart9@gmail.com>
2018-05-12 16:10:15 -0400

    build fix for libneo4j-client

--- scripts/linux/rhel/build_deps.sh
+++ scripts/linux/rhel/build_deps.sh
@@ -18,14 +18,14 @@ yum -y update
 printf "Addressing pre-build requirements"
 
 #Ensure that specific build requirements are satisfied
-yum -y install build-essential libtool pkg-config autoconf automake cmake make git wget gcc gcc-c++ epel-release
+yum -y install build-essential libtool pkg-config autoconf automake cmake make git wget gcc gcc-c++ epel-release openssl openssl-devel
 yum repolist
 
 #Determine if we need the neo4j-client library
 printf "Building libneo4j"
 wget https://github.com/cleishm/libneo4j-client/releases/download/v2.2.0/libneo4j-client-2.2.0.tar.gz
 tar -xvzf libneo4j-client-2.2.0.tar.gz
-cd libneo4j-client-2.2.0 && ./configure --disable-tools && make clean check && make install
+cd libneo4j-client-2.2.0 && ./configure --disable-tools && make && make install
 #cd /etc/yum.repos.d/ && sudo wget https://download.opensuse.org/repositories/home:cleishm/CentOS_7/home:cleishm.repo
 #sudo yum -y install libneo4j-client libneo4j-client-devel
 
